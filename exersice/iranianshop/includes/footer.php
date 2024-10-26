6248b8cf8da3c8ccd6838e865b90647e67604b69
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-10-26 10:07:14 +0330

    Create footer.php

---
+++ exersice/iranianshop/includes/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه ایرانیان</title>
+    <link rel="stylesheet" href="../tarnama.css" />
+</head>
+
+<body>
+    <div class="divTable">
+        <div class="divTableRow">
+            <footer class="divTable">
+                <section class="divTableRow">
+                    <article class="divTableCell">
+                        مالکیت مادی و معنوی سایت
+                    </article>
+                </section>
+            </footer>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
