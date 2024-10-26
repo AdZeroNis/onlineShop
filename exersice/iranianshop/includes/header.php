9cbd018f2a41e3589765f09372c9dacc6e415be7
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-10-26 10:07:20 +0330

    Create header.php

---
+++ exersice/iranianshop/includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه ایرانیان</title>
+    <link rel="stylesheet" href="../tarnama.css" />
+</head>
+
+<body>
+    <div class="divTable">
+        <div class="divTableRow">
+            <header class="divTable">
+                <div class="divTableRow">
+                    <div class="divTableCell">لوگوی سایت</div>
+                </div>
+            </header>
+            <nav class="divTable">
+                <ul class="divTableRow">
+                    <li class="divTableCell">
+                        <a class="set_style_link" href="index.php">صفحه اصلی</a>
+                    </li>
+                    <li class="divTableCell">
+                        <a class="set_style_link" href="#">عضویت در سایت</a>
+                    </li>
+                    <li class="divTableCell">
+                        <a class="set_style_link" href="#">ورود به سایت</a>
+                    </li>
+                    <li class="divTableCell">
+                        <a class="set_style_link" href="#">درباره ما</a>
+                    </li>
+                    <li class="divTableCell">
+                        <a class="set_style_link" href="#">ارتباط با ما</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</body>
+
+</html>
