9e94def17b010d872ef960974bc639846c37d347
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-10-26 10:07:24 +0330

    Create index.php

---
+++ exersice/iranianshop/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه ایرانیان</title>
+    <link rel="stylesheet" href="./tarnama.css" />
+</head>
+
+<body>
+    <div class="divTable">
+        <div class="divTableRow">
+            <div class="divTableCell">
+                <?php include './includes/header.php'; ?>
+                <section class="divTable">
+                    <section class="divTableRow">
+                        <aside class="divTableCell" style="width: 25%;">بخش امکانات سایت</aside>
+                        <section class="divTableCell" style="width: 75%;">محصولات</section>
+                    </section>
+                </section>
+                <?php include './includes/footer.php'; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
