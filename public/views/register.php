040375fa6f7f45200703ec92f86c7eb8c66b6233
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-11-14 18:15:57 +0330

    Create register.php

---
+++ public/views/register.php
@@ -0,0 +1,56 @@
+<?php include "./../php/register.php"; ?>
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../assets/css/style2.css">
+    <title>Ludiflex | Register</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="form-box">
+            <div class="register-container" id="register">
+                <div class="top">
+                    <span>حساب کاربری دارید؟ <a href="login.php">ورود</a></span>
+                    <img src="https://ibolak.com/storage/image/2024/6/1718807353-TCMlDRbPFbA1CHFG.svg" id="imgHeader" alt="">
+                </div>
+                <form action="" method="POST" onsubmit="return validateRegisterForm(event)">
+                    <div class="input-box">
+                        <input type="text" name="username" class="input-field" placeholder="نام کاربری" required title="فقط حروف مجاز است">
+                        <i class="bx bx-user"></i>
+                        <span id="usernameError" class="error-message"></span>
+                    </div>
+                    <div class="input-box">
+                        <input type="email" name="email" class="input-field" placeholder="ایمیل" required title="لطفا ایمیل معتبر وارد کنید">
+                        <i class="bx bx-envelope"></i>
+                        <span id="emailError" class="error-message"></span>
+                    </div>
+                    <div class="input-box">
+                        <input type="password" name="password" class="input-field" placeholder="کلمه عبور" required
+                            title="لطفا یک رمز عبور معتبر وارد کنید.(شامل حرف بزرگ،کوچک،عدد و عبارات خاص(@#&) و از 8 یا بیشتر کرکتر باشد)">
+                        <i class="bx bx-lock-alt"></i>
+                        <span id="passwordError" class="error-message"></span>
+                    </div>
+                    <div class="input-box">
+                        <input type="number" name="phone" class="input-field" placeholder="شماره تلفن" required
+                            title="لطفاً یک شماره تلفن معتبر (11 رقم که با 0 شروع می‌شود) وارد کنید">
+                        <i class="bx bx-phone"></i>
+                        <span id="PhoneError" class="error-message"></span>
+                    </div>
+                    <div class="input-box">
+                        <input type="submit" name="signup" class="submit" value="ثبت نام">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="../assets/js/js.js"></script>
+</body>
+
+</html>
