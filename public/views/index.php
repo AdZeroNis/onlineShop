dd0d8579042d2bccba81a99400877d746080725e
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-12-14 09:57:09 +0330

    Update index.php

--- public/views/index.php
+++ public/views/index.php
@@ -29,32 +29,213 @@ include '../php/index.php';
 
 <body>
   <?php include './header.php'; ?>
-  <div class="container my-4">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
-      <?php
-      if (count($products) > 0):
-        foreach ($products as $row): ?>
-          <div class="col">
-            <div class="card h-100 shadow-sm">
-              <img src="<?php echo ($row['image_path']); ?>" class="card-img-top" alt="تصویر محصول">
-              <div class="card-body">
-                <h5 class="card-title"><?php echo ($row['product_name']); ?></h5>
-                <p class="card-text">
-                  قیمت: <?php echo ($row['price']); ?> تومان
-                </p>
-                <a href="./singlePage.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">مشاهده جزئیات</a>
+  <div class="container">
+    <div class="banners my-1">
+      <div class="row">
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/link/redirect/dmq5e"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/7/1721731387-1OVhZaKqR0b4WYqF.webp
+                    " />
 
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/7/1721731387-1OVhZaKqR0b4WYqF.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/link/redirect/bnx9d"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/7/1721731387-7hucXpvrzgNGqXPj.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/7/1721731387-7hucXpvrzgNGqXPj.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/link/redirect/bp52d"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/7/1721731387-3eLaOlzuxAPhTNYd.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/7/1721731387-3eLaOlzuxAPhTNYd.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/link/redirect/eq3kb"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/7/1721816527-8TBF2MYdTQlotWyM.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/7/1721816527-8TBF2MYdTQlotWyM.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/shop/categories/dvqge/jeans"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/9/1725952416-0XVJCmDlrT3AP8L5.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/9/1725952416-0XVJCmDlrT3AP8L5.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/link/redirect/ew6gb"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/9/1725949731-VY1E0CpY8MXXOcgL.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/9/1725949731-VY1E0CpY8MXXOcgL.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/shop/categories/dyr5b/perfume"
+              title=""
+              target="_self">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/9/1725952416-tLIvWHRaNGdeHmmq.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/9/1725952416-tLIvWHRaNGdeHmmq.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="col-3 col-md-3 col-lg-3 my-2">
+          <div class="banner">
+            <a
+              href="https://ibolak.com/shop/categories/pdv6b/kids"
+              title=""
+              target="_blank">
+              <picture>
+                <!-- Source for desktop devices -->
+                <source
+                  media="(min-width: 1024px)"
+                  srcset="
+                      https://ibolak.com/storage/image/2024/9/1725949731-3BuGTMgqiKL85LRp.webp
+                    " />
+
+                <!-- Source for mobile devices -->
+                <img
+                  style="width: 100%"
+                  src="https://ibolak.com/storage/image/2024/9/1725949731-3BuGTMgqiKL85LRp.webp" />
+              </picture>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container my-4">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+        <?php
+        if (count($products) > 0):
+          foreach ($products as $row): ?>
+            <div class="col">
+              <div class="card h-100 shadow-sm">
+                <img src="<?php echo ($row['image_path']); ?>" class="card-img-top" alt="تصویر محصول">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo ($row['product_name']); ?></h5>
+                  <p class="card-text">
+                    قیمت: <?php echo ($row['price']); ?> تومان
+                  </p>
+                  <a href="./singlePage.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">مشاهده جزئیات</a>
+
+                </div>
               </div>
             </div>
-          </div>
+          <?php
+          endforeach;
+        else: ?>
+          <p>محصولی برای نمایش وجود ندارد.</p>
         <?php
-        endforeach;
-      else: ?>
-        <p>محصولی برای نمایش وجود ندارد.</p>
-      <?php
-      endif; ?>
+        endif; ?>
+      </div>
     </div>
-  </div>
   </div>
   <?php include './footer.php'; ?>
 
